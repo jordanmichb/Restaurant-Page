20f247e9b8289a3dbec0025772c43c5b9ceda596
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-08-23 17:55:25 -0700

    Adjust code so there is only one nav
    Simplify code by editing HTML to remove one of
    the nav sections and update the CSS and JS to
    accomodate this change while still functioning in
    the same way as before.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-const navSmall = document.querySelector(".nav-small");
+//const navSmall = document.querySelector(".nav-small");
+const nav = document.querySelector("nav");
 const hamburger = document.querySelector(".hamburger");
 
+
+function setNav() {
+    console.log("called")
+    const query = window.matchMedia("(max-width: 550px)");
+    if (query.matches) nav.classList.add("nav-small");
+    else nav.classList.remove("nav-small");
+}
+setNav();
+
+window.addEventListener('resize', setNav);
+
+
+
 hamburger.onclick = function() {
-    navSmall.classList.toggle("hidden");
-    navSmall.classList.toggle("visible");
+    //navSmall.classList.toggle("hidden");
+    nav.classList.toggle("visible");
 }
