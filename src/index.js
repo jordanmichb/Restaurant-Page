76b7c81ca553e84bb3360db6753d1dc17cbe5d8d
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-09-08 13:54:24 -0700

    Add JS comments

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-//const navSmall = document.querySelector(".nav-small");
 const nav = document.querySelector("nav");
 const menu = document.querySelector(".menu");
 
-
+/******************************************************/
+/* Controls how nav menu appears based on screen size */
+/******************************************************/
 function setNav() {
+    // On smaller screen, menu shows as hamburger icon
     const query = window.matchMedia("(max-width: 550px)");
     if (query.matches) nav.classList.add("nav-small");
+    // On larger screen, remove all styles reserved for smaller screen
     else {
         nav.classList.remove("nav-small");
         nav.classList.remove("visible");
         menu.classList.remove("active");
     }
 }
+// Initial call to set nav styles
 setNav();
 
+// Subsequent calls to set nav styles when screen is resized
 window.addEventListener('resize', setNav);
 
-
-
+// When icon is clicked, menu shows/hides and icon transforms to hamburger/X
 menu.onclick = function() {
-    //navSmall.classList.toggle("hidden");
     nav.classList.toggle("visible");
     menu.classList.toggle("active");
 }
