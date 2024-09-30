9ec9147fc229179b52078feae233f8545c28949f
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-09-29 18:19:02 -0700

    Add media query for menu and adjust price margin

--- src/index.js
+++ src/index.js
@@ -4,21 +4,23 @@ import loadMenu from'./menu.js';
 
 
 
-const nav = document.querySelector("nav");
-const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector('nav');
+const hamburger = document.querySelector('.hamburger');
+const homeBtn = document.querySelector('#home-btn');
+const menuBtn = document.querySelector('#menu-btn');
 
 /******************************************************/
 /* Controls how nav menu appears based on screen size */
 /******************************************************/
 function setNav() {
     // On smaller screen, menu shows as hamburger icon
-    const query = window.matchMedia("(max-width: 550px)");
-    if (query.matches) nav.classList.add("nav-small");
+    const query = window.matchMedia('(max-width: 550px)');
+    if (query.matches) nav.classList.add('nav-small');
     // On larger screen, remove all styles reserved for smaller screen
     else {
-        nav.classList.remove("nav-small");
-        nav.classList.remove("visible");
-        hamburger.classList.remove("active");
+        nav.classList.remove('nav-small');
+        nav.classList.remove('visible');
+        hamburger.classList.remove('active');
     }
 }
 // Initial call to set nav styles
@@ -29,9 +31,17 @@ window.addEventListener('resize', setNav);
 
 // When icon is clicked, menu shows/hides and icon transforms to hamburger/X
 hamburger.onclick = function() {
-    nav.classList.toggle("visible");
-    hamburger.classList.toggle("active");
+    nav.classList.toggle('visible');
+    hamburger.classList.toggle('active');
 }
 
-//loadHome();
-loadMenu();
+/************************************/
+/* Listeners for navigation buttons */
+/************************************/
+homeBtn.addEventListener('click', loadHome);
+menuBtn.addEventListener('click', loadMenu);
+
+
+
+
+loadHome();
