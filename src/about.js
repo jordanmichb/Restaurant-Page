f35e852ef0cabe07210d1e803ac8a2f92b5dbfd5
Jordan Ballard <95947696+jordanmichb@users.noreply.github.com>
2024-10-06 17:32:22 -0700

    Add text into info section, update styling

--- src/about.js
+++ src/about.js
@@ -1,11 +1,34 @@
 function createInfo() {
     const info = document.createElement('div');
     info.classList.add('info');
-    // Hours section
+
+    /***********************/
+    /* Conttact Section    */
+    /***********************/
+    const contactHeader = document.createElement('h3');
+    contactHeader.classList.add('component-header');
+    contactHeader.textContent = 'Contact';
+
+    const contact = document.createElement('div');
+    contact.classList.add('info-component');
+
+    const phone = document.createElement('span');
+    phone.textContent = '(123) 456 - 9780';
+
+    const email = document.createElement('span');
+    email.textContent = 'thevillage@gmail.com';
+
+    contact.appendChild(contactHeader);
+    contact.appendChild(phone);
+    contact.appendChild(email);
+
+    /**********************/
+    /* Hours Section      */
+    /**********************/
     const hours = document.createElement('div');
     hours.classList.add('info-component');
 
-    const hoursHeader = document.createElement('p');
+    const hoursHeader = document.createElement('h3');
     hoursHeader.classList.add('component-header');
     hoursHeader.textContent = 'Hours';
 
@@ -39,26 +62,32 @@ function createInfo() {
     hours.appendChild(sat);
     hours.appendChild(sun);
 
-    // Contact section
-    const contact = document.createElement('div');
-    contact.classList.add('info-component');
-
-    const contactHeader = document.createElement('p');
-    contactHeader.classList.add('component-header');
-    contactHeader.textContent = 'Contact';
-    contact.appendChild(contactHeader);
-
-    // Address section
+    /**********************/
+    /* Address Section    */
+    /**********************/
     const address = document.createElement('div');
     address.classList.add('info-component');
 
-    const addressHeader = document.createElement('p');
+    const addressHeader = document.createElement('h3');
     addressHeader.classList.add('component-header');
     addressHeader.textContent = 'Address';
-    address.appendChild(addressHeader);
 
-    info.appendChild(hours);
+    const adr = document.createElement('span');
+    adr.textContent = '123 Street Name'
+
+    const city = document.createElement('span');
+    city.textContent = 'City Name, XY, 50276'
+
+    const country = document.createElement('span');
+    country.textContent = 'United States'
+
+    address.appendChild(addressHeader);
+    address.appendChild(adr);
+    address.appendChild(city);
+    address.appendChild(country);
+
     info.appendChild(contact);
+    info.appendChild(hours);
     info.appendChild(address);
 
     return info;
@@ -74,6 +103,7 @@ function createAbout() {
 
     const text = document.createElement('p');
     text.classList.add('component-text');
+    text.classList.add('about-text');
     text.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '+
                         'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '+
                         'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '+
